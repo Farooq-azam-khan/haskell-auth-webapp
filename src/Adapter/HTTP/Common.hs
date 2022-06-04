6595a67148e7bd55126af38bf05ccab557ee7deb
Farooq <farooq1.khan@ryerson.ca>
2022-06-04 12:39:07 -0400

    cannot install digestive functors lib

---
+++ src/Adapter/HTTP/Common.hs
@@ -0,0 +1,74 @@
+module Adapter.HTTP.Common where 
+
+import ClassyPrelude
+import Web.Scotty.Tran
+import qualified Text.Digestive.Form as DF
+import qualified Text.Digestive.Types as DF
+import Data.Aeson hiding (json)
+import Network.HTTP.Types.Status
+import Blaze.ByteString.Builder (toLazyByteString)
+import Web.Cookie
+
+import Domain.Auth 
+import Data.Time.Lense 
+
+parseAndValidateJSOn :: (ScottyError e, MonadIO m, ToJSON v)
+                        => DF.Form v m a -> ActionT e m a
+parseAndValidateJSON form = do 
+        val <- jsonData `rescue` (\_ -> return Null)
+        validationResult <- lift $ DF.digestJSON form val 
+        case validationResult of 
+                (v, Nothing) -> 
+                        status status400
+                        json $ DF.jsonErrors v 
+                        finish
+                (_, Just result) -> 
+                        return result 
+
+toResult :: Either e a -> DF.Result e a
+toResult = either DF.Error DF.Success 
+
+setCookie :: (ScottyError e, Monad m) => SetCookie -> ActionT e m () 
+setcookie = 
+        setHeader "Set-Cookie" . decodeUtf8 . toLazyByteString . renderSetCookie 
+
+getCookie :: (ScottyError e, Monad m) => Text -> ActionT e m (Maybe Text)
+getCookie key = do 
+        mCookieStr <- header "Cookie"
+        return $ do 
+                cookie <- parseCookies . encodeUtf8 . toString <$> mCookieStr 
+                let bsKey = encodeUtf8 key 
+                val <- lookup bsKey cookie
+                return $ decodeUtf8 val 
+
+
+setSessionIdInCookie :: (MonadIO m, ScottyError e) => SessionId -> ActionT e m ()
+setSessionIdInCookie sId = do 
+        curTime <- liftIO getCurrentTime 
+        setCookie $ def { setCookieName = "sId"
+                        , setCookiePath = Just "/"
+                        , setCookieValue = encodeUtf8 sId
+                        , setCookieExpires = Just $ modL month (+ 1) curTime
+                        , setCookieHttpOnly = True
+                        , setCookieSecure = False
+                        , setCookieSameSite = Just sameSiteLax
+                        }
+
+getCurrentUserId :: (SessionRepo m, ScottyError e) => ActionT e m (Maybe UserId)
+getCurrentUserId = do 
+        maySessionId <- getCookie "sId"
+        case maySessionId of 
+                Nothing -> return Nothing
+                Just sId -> lift $ resolveSessionId sId
+
+reqCurrentUserId :: (SessionRepo m, ScottyError e) => ActionT e m UserId 
+reqCurrentUserId = do 
+        mayUserId <- getCurrentUserId 
+        case mayUserId of 
+                Nothing -> do 
+                        status status401
+                        json ("AuthRequred" :: Text)
+                        finish
+                Just userId -> 
+                        return userId
+
---
+++ src/Adapter/HTTP/Main.hs
@@ -0,0 +1,25 @@
+module Adapter.HTTP.Main where 
+
+import ClassyPrelude hiding (delete)
+import Web.Scotty.Trans
+import Network.HTTP.Types.Status
+import qualified Adapter.HTTP.API.Auth as AuthAPI
+import Adapter.HTTP.Common 
+import Katip 
+ipmort Network.Wai
+import Network.Wai.Middleware.Gzip
+
+main :: (MonadIO m, KatipContext m, AuthRepo m, EmailVerificationNotif m, SessionRepo m) 
+        => Int -> (m Response -> IO Response) -> IO ()
+main port runner = 
+        scottyT port runner routes
+
+routes :: (MonadIO m, KatipContext m, AuthRepo m, EmailVerificationNotif m, SessionRepo m) => ScottyT  LText m () 
+routes = do 
+        middleware $ gzip $ def {gzipFiles = GzipCompress } 
+        AuthApi.routes
+        defaultHandler $ \e -> do 
+                lift $ $(logTM) ErrorS $ "Unhandled error: " <> ls (showError e)
+                status status500
+                json ("InternalServerError" :: Text)
+
